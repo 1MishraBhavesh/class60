e33c80963b61eda5856f13419cbc785f0c9f43d7
1MishraBhavesh <74255631+1MishraBhavesh@users.noreply.github.com>
2021-04-18 13:38:21 +0530

    Add files via upload

---
+++ pro-c60-quiz-master-app/App.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import db from './config';
+
+export default class App extends Component {
+constructor(){
+  super();
+  this.state={
+    teamsRank:[],
+  };
+}
+ componentDidMount(){
+   this.showTeamsRanks();
+
+ }
+showTeamsRanks=()=>{
+  var teams=[];
+  var teamRef=db.ref('teams/');
+  teamRef.on("value",(data)=>{
+  var teamList= data.val();
+  console.log(teamList);
+   //console.log(teams[1]+'ruby');
+  for( var i in teamList){
+   
+       console.log(teamList[i]+ 'Bhavesh');
+    if(teamList[i]['isButtonPressed']===true){
+      teamList[i]['teamName']=i;
+      teams.push(teamList[i]);
+  
+         }
+    }
+   // console.log(teams+'ruby');
+
+      teams.sort(function(team1,team2){
+        return team1.timestamp-team2.timestamp
+      });
+      this.setState({
+        teamsRank:teams
+      });
+
+  })
+  
+}
+
+ resetDb =() => {
+   var lucky=db.ref('teams/').set({
+     blue: {
+     isButtonPressed:false,
+     timestamp:0
+     },
+      red: {
+     isButtonPressed:false,
+     timestamp:0
+     },
+     green: {
+     isButtonPressed:false,
+     timestamp:0
+     },
+      yellow: {
+     isButtonPressed:false,
+     timestamp:0
+     }
+   });
+
+   this.setState({teamsRank:[]});
+ }
+
+  render() {
+    return (
+      <View style={{flex:1}}>
+      <View style={{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+      }}>
+      {this.state.teamsRank.map((i)=>{
+        <View
+        style ={{
+          width: 140,
+          height:55,
+          borderWidth:2,
+          margin:5,
+          justifyContent:'center',
+          backgroundColor: i.teamName
+        }}>
+        <Text>{i.teamName.toUpperCase()}</Text>
+        </View>
+      })}
+       <Button title="reset" 
+       style={{width:100,height:100}}
+       onPress={this.resetDb}
+       />
+      </View>
+
+     
+      </View>
+    );
+  }
+}
